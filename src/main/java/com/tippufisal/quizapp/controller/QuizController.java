08a8db02f271d81feb1c1c24b235d4bf600be34a
Tippu <fisal17.tf@gmail.com>
2024-02-14 23:54:05 +0530

    Added Quiz module

---
+++ src/main/java/com/tippufisal/quizapp/controller/QuizController.java
@@ -0,0 +1,27 @@
+package com.tippufisal.quizapp.controller;
+
+import com.tippufisal.quizapp.model.wrapper.QuestionWrapper;
+import com.tippufisal.quizapp.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
+        return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizById(@PathVariable Integer id) {
+        return quizService.getQuizById(id);
+    }
+}
